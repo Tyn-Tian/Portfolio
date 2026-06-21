668fa33e107f396f19d94bc11315bec564e0a3c7
Christian <kristian2015ok@gmail.com>
2026-06-21 13:31:40 +0700

    refactor: replace custom SVG icons with Tabler icons for consistent styling

--- src/components/icons/tags/vercel.tsx
+++ src/components/icons/tags/vercel.tsx
@@ -1,14 +1,6 @@
+import { IconBrandVercel } from '@tabler/icons-react'
 import type { SVGProps } from 'react'
 
 export const VercelIcon = (props: SVGProps<SVGSVGElement>) => (
-  <svg
-    viewBox='0 0 256 222'
-    width='1em'
-    height='1em'
-    xmlns='http://www.w3.org/2000/svg'
-    preserveAspectRatio='xMidYMid'
-    {...props}
-  >
-    <path fill='currentColor' d='m128 0 128 221.705H0z' />
-  </svg>
+  <IconBrandVercel className={props.className} />
 )
